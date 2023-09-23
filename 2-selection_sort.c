e2679b089771e30062c455f0dc56379ab8689021
johnmariita <johnmariita421@gmail.com>
2023-09-23 21:40:56 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,7 +13,7 @@ void selection_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		small = array[i];
 		for (j = i + 1; j < size; j++)
